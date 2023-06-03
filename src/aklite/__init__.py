8a9aede1c212c73969c38094df849fb6b729b812
jindaxiang <jindaxiang@163.com>
2023-06-04 00:35:37 +0800

    init project

---
+++ src/aklite/__init__.py
@@ -0,0 +1,8 @@
+"""
+0.0.1 init project
+"""
+
+__version__ = "0.0.1"
+__author__ = "AKFamily"
+
+from aklite.stock.stock_hist_em import stock_zh_a_hist
---
+++ tests/test_ak.py
@@ -0,0 +1,4 @@
+import aklite as ai
+
+stock_hist_em_df = ai.stock_zh_a_hist()
+print(stock_hist_em_df)
