57559a5d9db196836b44968cde51725e7ec23459
jindaxiang <jindaxiang@163.com>
2023-06-04 20:00:16 +0800

    remove requests package

--- src/aklite/__init__.py
+++ src/aklite/__init__.py
@@ -2,9 +2,10 @@
 0.0.1 init project
 0.0.2 add stock_zh_a_hist interface
 0.0.3 add httpx package
+0.0.4 remove requests package
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "AKFamily"
 
 __slot__ = ["__version__", "__author__", "stock_zh_a_hist"]
